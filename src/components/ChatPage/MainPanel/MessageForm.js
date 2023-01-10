8c48defe08a7dda8b9d1bf69540801416dc319cb
jeonghyejin <109572328+JeongJinHye@users.noreply.github.com>
2023-01-10 23:07:55 +0900

    Update MessageForm.js

--- src/components/ChatPage/MainPanel/MessageForm.js
+++ src/components/ChatPage/MainPanel/MessageForm.js
@@ -57,12 +57,10 @@ function MessageForm() {
       return;
     }
     setLoading(true);
-    //firebase에 메시지를 저장하는 부분
+
     try {
-      // await messagesRef.child(chatRoom.id).push().set(createMessage())
       await set(push(child(messagesRef, chatRoom.id)), createMessage());
 
-      // typingRef.child(chatRoom.id).child(user.uid).remove();
       await remove(child(typingRef, `${chatRoom.id}/${user.uid}`));
       setLoading(false);
       setContent("");
@@ -97,16 +95,12 @@ function MessageForm() {
     const metadata = { contentType: file.type };
     setLoading(true);
     try {
-      // https://firebase.google.com/docs/storage/web/upload-files#full_example
-      // Upload file and metadata to the object 'images/mountains.jpg'
       const storageRef = strRef(storage, filePath);
       const uploadTask = uploadBytesResumable(storageRef, file, metadata);
 
-      // Listen for state changes, errors, and completion of the upload.
       uploadTask.on(
         "state_changed",
         (snapshot) => {
-          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
           const progress =
             (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
           console.log("Upload is " + progress + "% done");
@@ -120,27 +114,18 @@ function MessageForm() {
           }
         },
         (error) => {
-          // A full list of error codes is available at
-          // https://firebase.google.com/docs/storage/web/handle-errors
           switch (error.code) {
             case "storage/unauthorized":
-              // User doesn't have permission to access the object
               break;
             case "storage/canceled":
-              // User canceled the upload
               break;
 
-            // ...
-
             case "storage/unknown":
-              // Unknown error occurred, inspect error.serverResponse
               break;
           }
         },
         () => {
-          // Upload completed successfully, now we can get the download URL
           getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            // console.log('File available at', downloadURL);
             set(
               push(child(messagesRef, chatRoom.id)),
               createMessage(downloadURL)
