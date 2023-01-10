dc897ee639a430b487454658fa62facbe3e6397d
jeonghyejin <109572328+JeongJinHye@users.noreply.github.com>
2023-01-10 23:06:13 +0900

    Update UserPanel.js

--- src/components/ChatPage/SidePanel/UserPanel.js
+++ src/components/ChatPage/SidePanel/UserPanel.js
@@ -40,9 +40,8 @@ function UserPanel() {
 
     const metadata = { contentType: file.type };
     const storage = getStorage();
-    // https://firebase.google.com/docs/storage/web/upload-files#full_example
+    
     try {
-      //스토리지에 파일 저장하기
       let uploadTask = uploadBytesResumable(
         strRef(storage, `user_image/${user.uid}`),
         file,
@@ -52,7 +51,6 @@ function UserPanel() {
       uploadTask.on(
         "state_changed",
         (snapshot) => {
-          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
           const progress =
             (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
           console.log("Upload is " + progress + "% done");
@@ -66,25 +64,17 @@ function UserPanel() {
           }
         },
         (error) => {
-          // A full list of error codes is available at
-          // https://firebase.google.com/docs/storage/web/handle-errors
           switch (error.code) {
             case "storage/unauthorized":
-              // User doesn't have permission to access the object
               break;
             case "storage/canceled":
-              // User canceled the upload
               break;
 
-            // ...
-
             case "storage/unknown":
-              // Unknown error occurred, inspect error.serverResponse
               break;
           }
         },
         () => {
-          // Upload completed successfully, now we can get the download URL
           getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
             // 프로필 이미지 수정
             updateProfile(user, {
