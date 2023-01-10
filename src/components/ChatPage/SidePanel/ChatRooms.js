9add2657a8d5d4a85400f246827c4279052ffb06
jeonghyejin <109572328+JeongJinHye@users.noreply.github.com>
2023-01-10 23:05:10 +0900

    Update ChatRooms.js

--- src/components/ChatPage/SidePanel/ChatRooms.js
+++ src/components/ChatPage/SidePanel/ChatRooms.js
@@ -84,13 +84,11 @@ export class ChatRooms extends Component {
     DataSnapshot
   ) => {
     let lastTotal = 0;
-
-    // 이미 notifications state 안에 알림 정보가 들어있는 채팅방과 그렇지 않은 채팅방을 나눠주기
+    
     let index = notifications.findIndex(
       (notification) => notification.id === chatRoomId
     );
 
-    //notifications state 안에 해당 채팅방의 알림 정보가 없을 때
     if (index === -1) {
       notifications.push({
         id: chatRoomId,
@@ -99,24 +97,17 @@ export class ChatRooms extends Component {
         count: 0,
       });
     }
-    // 이미 해당 채팅방의 알림 정보가 있을 떄
-    else {
-      //상대방이 채팅 보내는 그 해당 채팅방에 있지 않을 때
-      if (chatRoomId !== currentChatRoomId) {
-        //현재까지 유저가 확인한 총 메시지 개수
-        lastTotal = notifications[index].lastKnownTotal;
 
-        //count (알림으로 보여줄 숫자)를 구하기
-        //현재 총 메시지 개수 - 이전에 확인한 총 메시지 개수 > 0
-        //현재 총 메시지 개수가 10개이고 이전에 확인한 메시지가 8개 였다면 2개를 알림으로 보여줘야함.
+    else {
+      if (chatRoomId !== currentChatRoomId) {
+        lastTotal = notifications[index].lastKnownTotal;
+        
         if (DataSnapshot.size - lastTotal > 0) {
           notifications[index].count = DataSnapshot.size - lastTotal;
         }
       }
-      //total property에 현재 전체 메시지 개수를 넣어주기
       notifications[index].total = DataSnapshot.size;
     }
-    //목표는 방 하나 하나의 맞는 알림 정보를 notifications state에  넣어주기
     this.setState({ notifications });
   };
 
@@ -167,7 +158,6 @@ export class ChatRooms extends Component {
   };
 
   getNotificationCount = (room) => {
-    //해당 채팅방의 count수를 구하는 중입니다.
     let count = 0;
 
     this.state.notifications.forEach((notification) => {
